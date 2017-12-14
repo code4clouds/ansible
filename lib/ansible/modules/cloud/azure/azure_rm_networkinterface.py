a8608a61e426a4d11fe3dc6f2db04e334733b62a
Raymond <siaomingjeng@users.noreply.github.com>
2017-12-14 15:54:57 +1100

    Update azure_rm_networkinterface.py
    delete the an unused line.

--- lib/ansible/modules/cloud/azure/azure_rm_networkinterface.py
+++ lib/ansible/modules/cloud/azure/azure_rm_networkinterface.py
@@ -6,7 +6,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
-from azure.mgmt.network.v2015_06_15.models.load_balancer import LoadBalancer
 __metaclass__ = type
 
 
